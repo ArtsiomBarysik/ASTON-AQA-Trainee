95f724fea1478801625b29206414edb9fcaca487
Garrnur <artsiombarysik@gmail.com>
2025-01-27 15:15:53 +0300

    оформление кода

--- HomeWork_15/src/test/java/homework/MtsMainPage.java
+++ HomeWork_15/src/test/java/homework/MtsMainPage.java
@@ -18,30 +18,30 @@ public class MtsMainPage extends BaseSeleniumPage {
     private WebElement agreeCookie;
 
     // Элементы фрейма оплаты
-    @FindBy (xpath = "//div[@class='pay-description__cost']//span[contains(text(), 'BYN')]")
+    @FindBy(xpath = "//div[@class='pay-description__cost']//span[contains(text(), 'BYN')]")
     private WebElement payDescriptionCost;
-    @FindBy (xpath = "//div[@class='pay-description__text']//span")
+    @FindBy(xpath = "//div[@class='pay-description__text']//span")
     private WebElement payDescriptionText;
-    @FindBy (xpath = "//button[@class='colored disabled']")
+    @FindBy(xpath = "//button[@class='colored disabled']")
     private WebElement payButtonDissabled;
-    @FindBy (xpath = "//iframe[@class='bepaid-iframe']")
+    @FindBy(xpath = "//iframe[@class='bepaid-iframe']")
     private WebElement bepaidIframe;
-    @FindBy (xpath = "//label[@class='ng-tns-c46-1 ng-star-inserted']")
+    @FindBy(xpath = "//label[@class='ng-tns-c46-1 ng-star-inserted']")
     private WebElement numberCardLabel;
-    @FindBy (xpath = "//label[@class='ng-tns-c46-4 ng-star-inserted']")
+    @FindBy(xpath = "//label[@class='ng-tns-c46-4 ng-star-inserted']")
     private WebElement durationCardLabel;
-    @FindBy (xpath = "//label[@class='ng-tns-c46-5 ng-star-inserted']")
+    @FindBy(xpath = "//label[@class='ng-tns-c46-5 ng-star-inserted']")
     private WebElement cvcCardLabel;
-    @FindBy (xpath = "//label[@class='ng-tns-c46-3 ng-star-inserted']")
+    @FindBy(xpath = "//label[@class='ng-tns-c46-3 ng-star-inserted']")
     private WebElement holderCardLabel;
-    @FindBy (xpath = "//div[@class='cards-brands ng-tns-c46-1']")
+    @FindBy(xpath = "//div[@class='cards-brands ng-tns-c46-1']")
     private WebElement payCardsLogoBlock;
-    @FindBy (xpath = "//img[@class='ng-tns-c61-0 ng-star-inserted']")
+    @FindBy(xpath = "//img[@class='ng-tns-c61-0 ng-star-inserted']")
     private List<WebElement> visiblePayCardsLogo;
     // Две карты которые имеют рандомное отображение
-    @FindBy (xpath = "//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']//img[1]")
+    @FindBy(xpath = "//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']//img[1]")
     private WebElement randomVisiblePayCardsLogoMaestro;
-    @FindBy (xpath = "//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']//img[2]")
+    @FindBy(xpath = "//div[@class='cards-brands cards-brands_random ng-tns-c61-0 ng-star-inserted']//img[2]")
     private WebElement randomVisiblePayCardsLogoMir;
 
     // Титл блока оплаты, партнеры оплаты, ссылка на подробную информацию
@@ -49,19 +49,19 @@ public class MtsMainPage extends BaseSeleniumPage {
     private WebElement paySectionTitle;
     @FindBy(xpath = "//div[contains(@class, 'pay__partners')]//img")
     private List<WebElement> payParthnersLogo;
-    @FindBy (xpath = "//div[@class='pay__wrapper']//a")
+    @FindBy(xpath = "//div[@class='pay__wrapper']//a")
     private WebElement moreInfoLink;
 
     // Селектор выбора услуг
-    @FindBy (xpath = "//button[@class='select__header']")
+    @FindBy(xpath = "//button[@class='select__header']")
     private WebElement buttonSelectServices;
-    @FindBy (xpath = "//p[text()='Услуги связи']")
+    @FindBy(xpath = "//p[text()='Услуги связи']")
     private WebElement selectPhoneServices;
-    @FindBy (xpath = "//p[text()='Домашний интернет']")
+    @FindBy(xpath = "//p[text()='Домашний интернет']")
     private WebElement selectInternetServices;
-    @FindBy (xpath = "//p[text()='Рассрочка']")
+    @FindBy(xpath = "//p[text()='Рассрочка']")
     private WebElement selectInstallmentServices;
-    @FindBy (xpath = "//p[text()='Задолженность']")
+    @FindBy(xpath = "//p[text()='Задолженность']")
     private WebElement selectArrearsServices;
 
     // Поля оплаты телефона
@@ -69,7 +69,7 @@ public class MtsMainPage extends BaseSeleniumPage {
     private WebElement phoneInputPhone;
     @FindBy(id = "connection-sum")
     private WebElement sumInputPhone;
-    @FindBy (id ="connection-email")
+    @FindBy(id = "connection-email")
     private WebElement emailInputPhone;
     @FindBy(xpath = "//*[@id='pay-connection']//button")
     private WebElement continueButtonPhone;
@@ -103,11 +103,12 @@ public class MtsMainPage extends BaseSeleniumPage {
     private WebElement emailInputArrears;
     @FindBy(xpath = "//*[@id='pay-arrears']//button")
     private WebElement continueButtonArrears;
+
     // Конструктор класса, отвечает за открытие страницы, инициализацию элементов, принятие куки файлов
     public MtsMainPage() {
         driver.get("https://www.mts.by");
         PageFactory.initElements(driver, this);
-        this.wait = new WebDriverWait(driver,5);
+        this.wait = new WebDriverWait(driver, 5);
         try {
             agreeCookie.click();
             System.out.println("Куки данные были приняты");
@@ -115,23 +116,26 @@ public class MtsMainPage extends BaseSeleniumPage {
             System.out.println("Окно с куки файлами не появилось");
         }
     }
+
     // Получение заголовка блока
     public String getBlockTitle() {
         return paySectionTitle.getText().replaceAll("\\s+", " ").trim();
     }
+
     // Проверка отображения всех логотипов
     public boolean arePaymentLogoDisplayed() {
         boolean allLogoFound = true;
         for (WebElement logo : payParthnersLogo) {
             if (logo.isDisplayed()) {
-                System.out.println("Логотип:" +logo.getAttribute("alt") + " найден");
+                System.out.println("Логотип:" + logo.getAttribute("alt") + " найден");
             } else {
-                System.out.println("Логотип:" +logo.getAttribute("alt") + " не найден");
+                System.out.println("Логотип:" + logo.getAttribute("alt") + " не найден");
                 allLogoFound = false;
             }
         }
         return allLogoFound;
     }
+
     // Клик на ссылку подробнее о сервисе
     public void clickMoreInfoLink() {
         moreInfoLink.click();
@@ -147,7 +151,7 @@ public class MtsMainPage extends BaseSeleniumPage {
         continueButtonPhone.click();
     }
 
-    public void switchToPayIframe(){
+    public void switchToPayIframe() {
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(bepaidIframe));
     }
 
@@ -161,62 +165,62 @@ public class MtsMainPage extends BaseSeleniumPage {
         return payButtonDissabled.getText();
     }
 
-    public String getDescriptionText(){
+    public String getDescriptionText() {
         wait.until(ExpectedConditions.visibilityOf(payDescriptionText));
         return payDescriptionText.getText();
     }
 
-    public boolean compareSumDescriptionOrder(String expectedSum){
+    public boolean compareSumDescriptionOrder(String expectedSum) {
         String actualSum = getDescriptionCost();
-        return expectedSum.equals(actualSum.replaceAll("\\.00 BYN",""));
+        return expectedSum.equals(actualSum.replaceAll("\\.00 BYN", ""));
     }
 
-    public boolean compareSumDescriptionButton(String expectedSum){
+    public boolean compareSumDescriptionButton(String expectedSum) {
         String actualSum = getButtonText();
-        return expectedSum.equals(actualSum.replaceAll("\\Оплатить ","").replaceAll("\\.00 BYN",""));
+        return expectedSum.equals(actualSum.replaceAll("\\Оплатить ", "").replaceAll("\\.00 BYN", ""));
     }
 
-    public boolean comparePhoneDescriptionOrder(String expectedPhone){
+    public boolean comparePhoneDescriptionOrder(String expectedPhone) {
         String actualPhone = getDescriptionText();
-        return expectedPhone.equals(actualPhone.replaceAll("\\Оплата: Услуги связи Номер:375",""));
+        return expectedPhone.equals(actualPhone.replaceAll("\\Оплата: Услуги связи Номер:375", ""));
     }
 
-    public String getNumberCardFieldLabel(){
+    public String getNumberCardFieldLabel() {
         wait.until(ExpectedConditions.visibilityOf(numberCardLabel));
         return numberCardLabel.getText();
     }
 
-    public String getDurationCardFieldLabel(){
+    public String getDurationCardFieldLabel() {
         wait.until(ExpectedConditions.visibilityOf(durationCardLabel));
         return durationCardLabel.getText();
     }
 
-    public String getCvcCardFieldLabel(){
+    public String getCvcCardFieldLabel() {
         wait.until(ExpectedConditions.visibilityOf(cvcCardLabel));
         return cvcCardLabel.getText();
     }
 
-    public String getHolderCardFieldLabel(){
+    public String getHolderCardFieldLabel() {
         wait.until(ExpectedConditions.visibilityOf(holderCardLabel));
         return holderCardLabel.getText();
     }
 
-    public boolean compareNumberLabel(String expectedNumberLabel){
+    public boolean compareNumberLabel(String expectedNumberLabel) {
         String actualNumberLabel = getNumberCardFieldLabel();
         return expectedNumberLabel.equals(actualNumberLabel);
     }
 
-    public boolean compareDurationLabel(String expectedDurationLabel){
+    public boolean compareDurationLabel(String expectedDurationLabel) {
         String actualDurationLabel = getDurationCardFieldLabel();
         return expectedDurationLabel.equals(actualDurationLabel);
     }
 
-    public boolean compareCvcLabel(String expectedCvcLabel){
+    public boolean compareCvcLabel(String expectedCvcLabel) {
         String actualCvcLabel = getCvcCardFieldLabel();
         return expectedCvcLabel.equals(actualCvcLabel);
     }
 
-    public boolean compareHolderLabel(String expectedHolderLabel){
+    public boolean compareHolderLabel(String expectedHolderLabel) {
         String actualHolderLabel = getHolderCardFieldLabel();
         return expectedHolderLabel.equals(actualHolderLabel);
     }
@@ -236,7 +240,7 @@ public class MtsMainPage extends BaseSeleniumPage {
         return allLogoFound;
     }
 
-    public boolean areRandomVisibleCardsLogoDisplayed(){
+    public boolean areRandomVisibleCardsLogoDisplayed() {
         System.out.println("Проверка сменяющихся логотипов:");
         boolean maestroLogoVisible = wait.until(ExpectedConditions.visibilityOf(randomVisiblePayCardsLogoMaestro)).isDisplayed();
         boolean mirLogoVisible = wait.until(ExpectedConditions.visibilityOf(randomVisiblePayCardsLogoMir)).isDisplayed();
@@ -249,82 +253,82 @@ public class MtsMainPage extends BaseSeleniumPage {
         return maestroLogoVisible && mirLogoVisible;
     }
 
-    public void selectPhoneServices(){
+    public void selectPhoneServices() {
         buttonSelectServices.click();
         selectPhoneServices.click();
     }
 
-    public boolean comparePhonePhonePlaceholder(String expectedPhonePlaceholder){
+    public boolean comparePhonePhonePlaceholder(String expectedPhonePlaceholder) {
         String actualPhonePlaceholder = phoneInputPhone.getAttribute("placeholder");
         return expectedPhonePlaceholder.equals(actualPhonePlaceholder);
     }
 
-    public boolean comparePhoneSumPlaceholder(String expectedSumPlaceholder){
+    public boolean comparePhoneSumPlaceholder(String expectedSumPlaceholder) {
         String actualSumPlaceholder = sumInputPhone.getAttribute("placeholder");
         return expectedSumPlaceholder.equals(actualSumPlaceholder);
     }
 
-    public boolean comparePhoneEmailPlaceholder(String expectedEmailPlaceholder){
+    public boolean comparePhoneEmailPlaceholder(String expectedEmailPlaceholder) {
         String actualEmailPlaceholder = emailInputPhone.getAttribute("placeholder");
         return expectedEmailPlaceholder.equals(actualEmailPlaceholder);
     }
 
-    public void selectInternetServices(){
+    public void selectInternetServices() {
         buttonSelectServices.click();
         selectInternetServices.click();
     }
 
-    public boolean compareInternetPhonePlaceholder(String expectedPhonePlaceholder){
+    public boolean compareInternetPhonePlaceholder(String expectedPhonePlaceholder) {
         String actualPhonePlaceholder = phoneInputInternet.getAttribute("placeholder");
         return expectedPhonePlaceholder.equals(actualPhonePlaceholder);
     }
 
-    public boolean compareInternetSumPlaceholder(String expectedSumPlaceholder){
+    public boolean compareInternetSumPlaceholder(String expectedSumPlaceholder) {
         String actualSumPlaceholder = sumInputInternet.getAttribute("placeholder");
         return expectedSumPlaceholder.equals(actualSumPlaceholder);
     }
 
-    public boolean compareInternetEmailPlaceholder(String expectedEmailPlaceholder){
+    public boolean compareInternetEmailPlaceholder(String expectedEmailPlaceholder) {
         String actualEmailPlaceholder = emailInputInternet.getAttribute("placeholder");
         return expectedEmailPlaceholder.equals(actualEmailPlaceholder);
     }
 
-    public void selectInstallmentServices(){
+    public void selectInstallmentServices() {
         buttonSelectServices.click();
         selectInstallmentServices.click();
     }
 
-    public boolean compareInstallmentPhonePlaceholder(String expectedPhonePlaceholder){
+    public boolean compareInstallmentPhonePlaceholder(String expectedPhonePlaceholder) {
         String actualPhonePlaceholder = phoneInputInstallment.getAttribute("placeholder");
         return expectedPhonePlaceholder.equals(actualPhonePlaceholder);
     }
 
-    public boolean compareInstallmentSumPlaceholder(String expectedSumPlaceholder){
+    public boolean compareInstallmentSumPlaceholder(String expectedSumPlaceholder) {
         String actualSumPlaceholder = sumInputInstallment.getAttribute("placeholder");
         return expectedSumPlaceholder.equals(actualSumPlaceholder);
     }
 
-    public boolean compareInstallmentEmailPlaceholder(String expectedEmailPlaceholder){
+    public boolean compareInstallmentEmailPlaceholder(String expectedEmailPlaceholder) {
         String actualEmailPlaceholder = emailInputInstallment.getAttribute("placeholder");
         return expectedEmailPlaceholder.equals(actualEmailPlaceholder);
     }
 
-    public void selectArrearsServices(){
+    public void selectArrearsServices() {
         buttonSelectServices.click();
         selectArrearsServices.click();
     }
 
-    public boolean compareArrearsPhonePlaceholder(String expectedPhonePlaceholder){
+    public boolean compareArrearsPhonePlaceholder(String expectedPhonePlaceholder) {
         String actualPhonePlaceholder = phoneInputArrears.getAttribute("placeholder");
         return expectedPhonePlaceholder.equals(actualPhonePlaceholder);
     }
 
-    public boolean compareArrearsSumPlaceholder(String expectedSumPlaceholder){
+    public boolean compareArrearsSumPlaceholder(String expectedSumPlaceholder) {
         String actualSumPlaceholder = sumInputArrears.getAttribute("placeholder");
         return expectedSumPlaceholder.equals(actualSumPlaceholder);
     }
 
-    public boolean compareArrearsEmailPlaceholder(String expectedEmailPlaceholder){
+    public boolean compareArrearsEmailPlaceholder(String expectedEmailPlaceholder) {
         String actualEmailPlaceholder = emailInputArrears.getAttribute("placeholder");
         return expectedEmailPlaceholder.equals(actualEmailPlaceholder);
     }
