dc2efddee64425c597b69ae5b64b56c27f4e5aec
Garrnur <artsiombarysik@gmail.com>
2025-02-03 22:52:59 +0300

    Доработка

--- HomeWork_15/src/main/java/org/example/pages/MtsMainPage.java
+++ HomeWork_15/src/main/java/org/example/pages/MtsMainPage.java
@@ -1,4 +1,4 @@
-package homework;
+package org.example.pages;
 
 import core.BaseSeleniumPage;
 import org.openqa.selenium.WebElement;
--- HomeWork_15/src/test/java/tests/MtsMainPageTest.java
+++ HomeWork_15/src/test/java/tests/MtsMainPageTest.java
@@ -1,7 +1,8 @@
-package homework;
+package tests;
 
 import core.BaseSeleniumTest;
 import core.TestListener;
+import org.example.pages.MtsMainPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,34 +13,31 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisplayName("Тесты блока 'Онлайн пополнение без комиссии'")
 public class MtsMainPageTest extends BaseSeleniumTest {
     // Проверочные данные Заголовка и ссылка на подробнее о сервисе
-    static String expectedTitle = "Онлайн пополнение без комиссии";
-    static String moreInfoUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+    private static final String EXPECTED_TITLE = "Онлайн пополнение без комиссии";
+    private static final String MORE_INFO_URL = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
     // Тестовый номер и сумма
-    static String phone = "297777777";
-    static String sum = "20";
+    private static final String TEST_PHONE = "297777777";
+    private static final String TEST_SUM = "20";
     // Проверочные данные для данных карты в форме оплаты
-    static String expectedNumberCard = "Номер карты";
-    static String expectedDurationCard = "Срок действия";
-    static String expectedCvcCard ="CVC";
-    static String expectedHolderCard ="Имя держателя (как на карте)";
+    private static final String EXPECTED_NUMBER_CARD = "Номер карты";
+    private static final String EXPECTED_DURATION_CARD = "Срок действия";
+    private static final String EXPECTED_CVC_CARD ="CVC";
+    private static final String EXPECTED_HOLDER_CARD ="Имя держателя (как на карте)";
     // Проверочные плейсхолдеры номера телефона/счета для вариантов услуг
-    static String expectedPhonePhonePlaceholder ="Номер телефона";
-    static String expectedInternetPhonePlaceholder ="Номер абонента";
-    static String expectedInstallmentPhonePlaceholder ="Номер счета на 44";
-    static String expectedArrearsPhonePlaceholder ="Номер счета на 2073";
+    private static final String EXPECTED_PHONE_PHONE_PLACEHOLDER ="Номер телефона";
+    private static final String EXPECTED_INTERNET_PHONE_PLACEHOLDER ="Номер абонента";
+    private static final String EXPECTED_INSTALLMENT_PHONE_PLACEHOLDER ="Номер счета на 44";
+    private static final String EXPECTED_ARREARS_PHONE_PLACEHOLDER ="Номер счета на 2073";
     // Проверочные плейсхолдеры для полей суммы и емайла для всех вариантов услуг
-    static String expectedSumPlaceholder ="Сумма";
-    static String expectedEmailPlaceholder ="E-mail для отправки чека";
+    private static final String EXPECTED_SUM_PLACEHOLDER ="Сумма";
+    private static final String EXPECTED_EMAIL_PLACEHOLDER ="E-mail для отправки чека";
 
     @Test
     @DisplayName("Проверка названия блока")
     public void checkTitleBlock(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         String actualTitle = mtsMainPage.getBlockTitle();
-        assertEquals(expectedTitle, actualTitle);
-        if (expectedTitle.equals(actualTitle)) {
-            System.out.println("Тест прошел, название соответствует");
-        }
+        assertEquals(EXPECTED_TITLE, actualTitle);
     }
 
     @Test
@@ -54,7 +52,7 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     public void checkMoreInfoLink(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         mtsMainPage.clickMoreInfoLink();
-        assertTrue(driver.getCurrentUrl().contains(moreInfoUrl));
+        assertTrue(driver.getCurrentUrl().contains(MORE_INFO_URL));
         driver.navigate().back();
     }
 
@@ -62,34 +60,31 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     @DisplayName("Проверка заполнения формы оплаты телефона")
     public void checkPaymentPhone() {
         MtsMainPage mtsMainPage = new MtsMainPage();
-        mtsMainPage.fillAndSubmitForm(phone,sum);
+        mtsMainPage.fillAndSubmitForm(TEST_PHONE,TEST_SUM);
         mtsMainPage.switchToPayIframe();
-
-        assertTrue(mtsMainPage.compareSumDescriptionOrder(sum));
-        assertTrue(mtsMainPage.compareSumDescriptionButton(sum));
-        assertTrue(mtsMainPage.comparePhoneDescriptionOrder(phone));
+        assertTrue(mtsMainPage.compareSumDescriptionOrder(TEST_SUM));
+        assertTrue(mtsMainPage.compareSumDescriptionButton(TEST_SUM));
+        assertTrue(mtsMainPage.comparePhoneDescriptionOrder(TEST_PHONE));
     }
 
     @Test
     @DisplayName("Проверка плэйсходеров в форме оплаты телефона")
     public void checkPlaceholderPhone(){
         MtsMainPage mtsMainPage = new MtsMainPage();
-        mtsMainPage.fillAndSubmitForm(phone,sum);
+        mtsMainPage.fillAndSubmitForm(TEST_PHONE,TEST_SUM);
         mtsMainPage.switchToPayIframe();
-
-        assertTrue(mtsMainPage.compareNumberLabel(expectedNumberCard));
-        assertTrue(mtsMainPage.compareDurationLabel(expectedDurationCard));
-        assertTrue(mtsMainPage.compareCvcLabel(expectedCvcCard));
-        assertTrue(mtsMainPage.compareHolderLabel(expectedHolderCard));
+        assertTrue(mtsMainPage.compareNumberLabel(EXPECTED_NUMBER_CARD));
+        assertTrue(mtsMainPage.compareDurationLabel(EXPECTED_DURATION_CARD));
+        assertTrue(mtsMainPage.compareCvcLabel(EXPECTED_CVC_CARD));
+        assertTrue(mtsMainPage.compareHolderLabel(EXPECTED_HOLDER_CARD));
     }
 
     @Test
     @DisplayName("Проверка логотипов платежных карт в форме оплаты")
     public void checkLogoCardOrder(){
         MtsMainPage mtsMainPage = new MtsMainPage();
-        mtsMainPage.fillAndSubmitForm(phone,sum);
+        mtsMainPage.fillAndSubmitForm(TEST_PHONE,TEST_SUM);
         mtsMainPage.switchToPayIframe();
-
         assertTrue(mtsMainPage.areVisibleCardsLogoDisplayed());
         assertTrue(mtsMainPage.areRandomVisibleCardsLogoDisplayed());
     }
@@ -99,9 +94,9 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     public void checkPlaceholdersPhone(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         mtsMainPage.selectPhoneServices();
-        assertTrue(mtsMainPage.comparePhonePhonePlaceholder(expectedPhonePhonePlaceholder));
-        assertTrue(mtsMainPage.comparePhoneSumPlaceholder(expectedSumPlaceholder));
-        assertTrue(mtsMainPage.comparePhoneEmailPlaceholder(expectedEmailPlaceholder));
+        assertTrue(mtsMainPage.comparePhonePhonePlaceholder(EXPECTED_PHONE_PHONE_PLACEHOLDER));
+        assertTrue(mtsMainPage.comparePhoneSumPlaceholder(EXPECTED_SUM_PLACEHOLDER));
+        assertTrue(mtsMainPage.comparePhoneEmailPlaceholder(EXPECTED_EMAIL_PLACEHOLDER));
     }
 
     @Test
@@ -109,9 +104,9 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     public void checkPlaceholdersInternet(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         mtsMainPage.selectInternetServices();
-        assertTrue(mtsMainPage.compareInternetPhonePlaceholder(expectedInternetPhonePlaceholder));
-        assertTrue(mtsMainPage.compareInternetSumPlaceholder(expectedSumPlaceholder));
-        assertTrue(mtsMainPage.compareInternetEmailPlaceholder(expectedEmailPlaceholder));
+        assertTrue(mtsMainPage.compareInternetPhonePlaceholder(EXPECTED_INTERNET_PHONE_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareInternetSumPlaceholder(EXPECTED_SUM_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareInternetEmailPlaceholder(EXPECTED_EMAIL_PLACEHOLDER));
     }
 
     @Test
@@ -119,9 +114,9 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     public void checkPlaceholdersInstallment(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         mtsMainPage.selectInstallmentServices();
-        assertTrue(mtsMainPage.compareInstallmentPhonePlaceholder(expectedInstallmentPhonePlaceholder));
-        assertTrue(mtsMainPage.compareInstallmentSumPlaceholder(expectedSumPlaceholder));
-        assertTrue(mtsMainPage.compareInstallmentEmailPlaceholder(expectedEmailPlaceholder));
+        assertTrue(mtsMainPage.compareInstallmentPhonePlaceholder(EXPECTED_INSTALLMENT_PHONE_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareInstallmentSumPlaceholder(EXPECTED_SUM_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareInstallmentEmailPlaceholder(EXPECTED_EMAIL_PLACEHOLDER));
     }
 
     @Test
@@ -129,8 +124,8 @@ public class MtsMainPageTest extends BaseSeleniumTest {
     public void checkPlaceholdersArrears(){
         MtsMainPage mtsMainPage = new MtsMainPage();
         mtsMainPage.selectArrearsServices();
-        assertTrue(mtsMainPage.compareArrearsPhonePlaceholder(expectedArrearsPhonePlaceholder));
-        assertTrue(mtsMainPage.compareArrearsSumPlaceholder(expectedSumPlaceholder));
-        assertTrue(mtsMainPage.compareArrearsEmailPlaceholder(expectedEmailPlaceholder));
+        assertTrue(mtsMainPage.compareArrearsPhonePlaceholder(EXPECTED_ARREARS_PHONE_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareArrearsSumPlaceholder(EXPECTED_SUM_PLACEHOLDER));
+        assertTrue(mtsMainPage.compareArrearsEmailPlaceholder(EXPECTED_EMAIL_PLACEHOLDER));
     }
 }
